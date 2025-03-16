c62a54f230b83c7c732b928429e870e887648baf
Murodbek <arslonovmurodbek071@gmail.com>
2025-03-16 10:53:17 +0500

    newwss

--- js/main.js
+++ js/main.js
@@ -39,3 +39,6 @@ const scrollTopBtn = document.getElementById("scrollTopBtn");
 scrollTopBtn.addEventListener("click", () => {
   window.scrollTo({ top: 0, behavior: "smooth" });
 });
+
+// Aos
+AOS.init();
