f33bed2d62f48783ea8a3e70bcac785ca0ad8f7a
Murodbek <arslonovmurodbek071@gmail.com>
2025-03-16 09:27:58 +0500

    new pages media

--- js/main.js
+++ js/main.js
@@ -1,46 +1,32 @@
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    items: 1, // Bitta rasm ko‘rsatish
+    loop: true, // Cheksiz aylanish
+    nav: false, // Navigatsiya tugmalari
+    dots: true, //flase Nuqtalar (indicator)
+    autoplay: true, // Avtomatik aylanish
+    autoplayTimeout: 3000, // 3 soniyada slayder o‘zgaradi
+    responsive: {
+      0: { items: 1.8 },
+      600: { items: 3.5 },
+      1000: { items: 6 },
+    },
+  });
+});
 // dark-mode
 
 let modeBtn = document.getElementById("mode-btn");
 
-modeBtn.addEventListener("click", function () {
+document.addEventListener("click", function () {
   document.body.classList.toggle("dark");
 });
-
-//back to top
-
-let mybutton = document.getElementById("myBtn");
-
-window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-const myBtn = document.getElementById("myBtn");
-myBtn.onclick = function () {
-  const scrollStep = -window.scrollY / (10 / 180);
-  const scrollInterval = setInterval(() => {
-    if (window.scrollY === 0) {
-      clearInterval(scrollInterval);
-    } else {
-      window.scrollBy(0, scrollStep);
-    }
-  }, 15);
-};
 
 // navbar shrink
 
 window.addEventListener("scroll", function () {
   shrink();
 });
-
 let navbar = document.getElementById("header");
-
 function shrink() {
   if (scrollY > 100) {
     navbar.classList.add("navbar-shrink");
@@ -48,28 +34,3 @@ function shrink() {
     navbar.classList.remove("navbar-shrink");
   }
 }
-
-// loading
-
-const loading = document.getElementById("loading");
-
-const loadingDuration = 2000; // 1.8s
-
-setTimeout(() => {
-  loading.classList.add("loading-none");
-}, loadingDuration);
-
-//hamburger
-
-document.addEventListener("DOMContentLoaded", () => {
-  const menuToggle = document.getElementById("hamburger");
-  const navLinks = document.getElementById("nav-menus");
-
-  menuToggle.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
-  });
-});
-
-//Aos
-
-AOS.init();
