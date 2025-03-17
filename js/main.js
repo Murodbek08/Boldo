6263d69c0e83adcc837d87327c69291ee80b5f09
Murodbek <arslonovmurodbek071@gmail.com>
2025-03-17 14:07:52 +0500

    new page shhobiddin

--- js/main.js
+++ js/main.js
@@ -1,15 +1,33 @@
 $(document).ready(function () {
-  $(".owl-carousel").owlCarousel({
+  $(".owl-carousel-1").owlCarousel({
     items: 1, // Bitta rasm ko‘rsatish
     loop: true, // Cheksiz aylanish
     nav: false, // Navigatsiya tugmalari
     dots: true, //flase Nuqtalar (indicator)
     autoplay: true, // Avtomatik aylanish
-    autoplayTimeout: 3000, // 3 soniyada slayder o‘zgaradi
+    autoplayTimeout: 1000, // 3 soniyada slayder o‘zgaradi
+    autoplayHoverPause: true,
     responsive: {
       0: { items: 1.8 },
       600: { items: 3.5 },
-      1000: { items: 6 },
+      1000: { items: 5 },
+    },
+  });
+});
+
+$(document).ready(function () {
+  $(".owl-carousel-2").owlCarousel({
+    items: 1, // Bitta rasm ko‘rsatish
+    loop: true, // Cheksiz aylanish
+    nav: false, // Navigatsiya tugmalari
+    dots: false, //flase Nuqtalar (indicator)
+    autoplay: true, // Avtomatik aylanish
+    autoplayTimeout: 1000, // 3 soniyada slayder o‘zgaradi
+    autoplayHoverPause: true,
+    responsive: {
+      0: { items: 1 },
+      600: { items: 2 },
+      1000: { items: 3 },
     },
   });
 });
